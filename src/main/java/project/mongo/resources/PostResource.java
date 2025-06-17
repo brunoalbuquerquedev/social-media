ea62ce46b5f71f15ee8189440098876ce33ee801
Bruno Albuquerque <albuquerquebruno2020@gmail.com>
2025-06-17 01:44:43 -0300

    get post by id

--- src/main/java/project/mongo/resources/PostResource.java
+++ src/main/java/project/mongo/resources/PostResource.java
@@ -3,6 +3,7 @@ package project.mongo.resources;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import project.mongo.domain.Post;
@@ -21,5 +22,11 @@ public class PostResource {
     public ResponseEntity<List<Post>> findAll() {
         List<Post> list = postService.findAll();
         return ResponseEntity.ok().body(list);
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<Post> findById(@PathVariable String id) {
+        Post post = postService.findById(id);
+        return ResponseEntity.ok().body(post);
     }
 }
--- src/main/java/project/mongo/services/PostService.java
+++ src/main/java/project/mongo/services/PostService.java
@@ -4,6 +4,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import project.mongo.domain.Post;
 import project.mongo.repository.PostRepository;
+import project.mongo.services.exceptions.ObjectNotFoundException;
 
 import java.util.List;
 
@@ -15,5 +16,9 @@ public class PostService {
 
     public List<Post> findAll() {
         return postRepository.findAll();
+    }
+
+    public Post findById(String id) {
+        return postRepository.findById(id).orElseThrow(() -> new ObjectNotFoundException("Post not found."));
     }
 }
